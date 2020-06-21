da4345c73a59135333b8bf370b54c531ed21ab3c
Dawid Gal <d.gal@poczta.onet.pl>
2020-06-21 09:43:11 +0200

    Added the solution to the exercise

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,17 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int sum = 0;
+
+            for (int i = 1; i <= 20; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+
+            Console.WriteLine($"The sum is: {sum}");
         }
     }
 }
